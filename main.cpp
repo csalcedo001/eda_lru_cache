b3a70b18c5f2c7da2ddf7e06e42fd8f385a91f8c
César Salcedo <cesar.salcedo@utec.edu.pe>
2020-09-11 12:31:57 -0500

    Better documentation of main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <climits>
 
 #include <lru_cache.hpp>
 
@@ -8,23 +7,33 @@ using namespace std;
 int main() {
 	eda::LRUCache<std::string, int> cache(3);
 
+	cout << "Keys 'b', 'a' and 'c' inserted in that order" << endl;
+
+	// Insert multiple elements to the cache
 	cache.insertKeyValuePair("b", 2);
 	cache.insertKeyValuePair("a", 1);
 	cache.insertKeyValuePair("c", 3);
 
-	cout << cache.getMostRecentKey() << endl;
+	// Print the most recently accessed key
+	cout << "Most recently accessed key: " << cache.getMostRecentKey() << endl;
 
-	cout << cache.getValueFromKey("a").value_or(INT_MIN) << endl;
+	// Get a value of a key
+	cout << "Get value from 'a': " << cache.getValueFromKey("a").value_or(NULL) << endl;
 
-	cout << cache.getMostRecentKey() << endl;
+	// Accessing a value updates the most recently accessed key
+	cout << "New most recently accessed key: " << cache.getMostRecentKey() << endl;
 
+	// When cache is full, one key is evicted
 	cache.insertKeyValuePair("d", 4);
 
-	cout << cache.getValueFromKey("b").value_or(INT_MIN) << endl;
+	// Since 'b' was the least recently used key, it is no longer in the cache
+	cout << "Trying to access evicted key 'b': " << cache.getValueFromKey("b").value_or(NULL) << endl;
 
+	cout << "Update value of 'a' to 5" << endl;
+
+	// Modify value of an existing key
 	cache.insertKeyValuePair("a", 5);
-
-	cout << cache.getValueFromKey("a").value_or(INT_MIN) << endl;
+	cout << "Get value from 'a': " << cache.getValueFromKey("a").value_or(NULL) << endl;
 
 	return 0;
 }
